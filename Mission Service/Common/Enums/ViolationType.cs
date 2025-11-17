8d1156e4915d4a0394f949c9a3eba3083f710444
kalush666 <jonathan.kalush@gmail.com>
2025-11-17 18:03:09 +0200

    [feat] violations

---
+++ Mission Service/Common/Enums/ViolationType.cs
@@ -0,0 +1,10 @@
+﻿namespace Mission_Service.Common.Enums
+{
+    public enum ViolationType
+    {
+        TIME_OVERLAP,
+        TYPE_MISMATCH,
+        FUEL_SHORTAGE,
+        WINDOW_VIOLATION
+    }
+}
---
+++ Mission Service/Models/Violation.cs
@@ -0,0 +1,18 @@
+﻿using Mission_Service.Common.Enums;
+
+namespace Mission_Service.Models
+{
+    public class Violation
+    {
+        public ViolationType ViolationType { get; set; }
+        public SeveretyLevel SeveretyLevel { get; set; }
+        public AssignmentGene AssignmentGene { get; set; }
+        public Violation() { }
+        public Violation(ViolationType violationType, SeveretyLevel severetyLevel, AssignmentGene assignmentGene)
+        {
+            ViolationType = violationType;
+            SeveretyLevel = severetyLevel;
+            AssignmentGene = assignmentGene;
+        }
+    }
+}
