135f7cfa06a2cc04291c36c32d9a2b9396e0a7a9
kalush666 <jonathan.kalush@gmail.com>
2025-11-18 13:36:59 +0200

    [refactor] clean code

--- Mission Service/Extensions/Normalization.cs
+++ Mission Service/Extensions/Normalization.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Common.Enums;
+using Mission_Service.Common.Constants;
 
 namespace Mission_Service.Extensions
 {
@@ -8,26 +9,26 @@ namespace Mission_Service.Extensions
         {
             return field switch
             {
-                TelemetryFields.FuelAmount => value / 100.0,
-                TelemetryFields.ThrottlePercent => value / 100.0,
-                TelemetryFields.CurrentSpeedKmph => Math.Min(value / 4095.0, 1.0),
-                TelemetryFields.Altitude => Math.Min(value / 65535.0, 1.0),
-                TelemetryFields.CruiseAltitude => Math.Min(value / 65535.0, 1.0),
-                TelemetryFields.SignalStrength => (value + 200.0) / 400.0,
-                TelemetryFields.DataStorageUsedGB => 1.0 - Math.Min(value / 10485.75, 1.0),
-                TelemetryFields.FlightTimeSec => Math.Min(value / 1048575.0, 1.0),
-                TelemetryFields.ThrustAfterInfluence => Math.Min(value / 1048575.0, 1.0),
-                TelemetryFields.EngineDegrees => 1.0 - Math.Min(value / 524.287, 1.0),
-                TelemetryFields.Rpm => Math.Min(value / 65535.0, 1.0),
-                TelemetryFields.YawDeg => value / 360.0,
-                TelemetryFields.PitchDeg => (value + 90.0) / 180.0,
-                TelemetryFields.RollDeg => (value + 90.0) / 180.0,
-                TelemetryFields.DragCoefficient => 1.0 - Math.Min(value / 6.5535, 1.0),
-                TelemetryFields.LiftCoefficient => Math.Min(value / 6.5535, 1.0),
+                TelemetryFields.FuelAmount => value / MissionServiceConstants.TelemetryNormalization.MAX_PERCENTAGE,
+                TelemetryFields.ThrottlePercent => value / MissionServiceConstants.TelemetryNormalization.MAX_PERCENTAGE,
+                TelemetryFields.CurrentSpeedKmph => Math.Min(value / MissionServiceConstants.TelemetryNormalization.MAX_SPEED_KMPH, MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE),
+                TelemetryFields.Altitude => Math.Min(value / MissionServiceConstants.TelemetryNormalization.MAX_ALTITUDE, MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE),
+                TelemetryFields.CruiseAltitude => Math.Min(value / MissionServiceConstants.TelemetryNormalization.MAX_ALTITUDE, MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE),
+                TelemetryFields.SignalStrength => (value + MissionServiceConstants.TelemetryNormalization.SIGNAL_STRENGTH_OFFSET) / MissionServiceConstants.TelemetryNormalization.SIGNAL_STRENGTH_RANGE,
+                TelemetryFields.DataStorageUsedGB => MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE - Math.Min(value / MissionServiceConstants.TelemetryNormalization.MAX_DATA_STORAGE_GB, MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE),
+                TelemetryFields.FlightTimeSec => Math.Min(value / MissionServiceConstants.TelemetryNormalization.MAX_FLIGHT_TIME_SEC, MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE),
+                TelemetryFields.ThrustAfterInfluence => Math.Min(value / MissionServiceConstants.TelemetryNormalization.MAX_THRUST, MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE),
+                TelemetryFields.EngineDegrees => MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE - Math.Min(value / MissionServiceConstants.TelemetryNormalization.MAX_ENGINE_DEGREES, MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE),
+                TelemetryFields.Rpm => Math.Min(value / MissionServiceConstants.TelemetryNormalization.MAX_RPM, MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE),
+                TelemetryFields.YawDeg => value / MissionServiceConstants.TelemetryNormalization.FULL_ROTATION_DEG,
+                TelemetryFields.PitchDeg => (value + MissionServiceConstants.TelemetryNormalization.PITCH_ROLL_OFFSET) / MissionServiceConstants.TelemetryNormalization.PITCH_ROLL_RANGE,
+                TelemetryFields.RollDeg => (value + MissionServiceConstants.TelemetryNormalization.PITCH_ROLL_OFFSET) / MissionServiceConstants.TelemetryNormalization.PITCH_ROLL_RANGE,
+                TelemetryFields.DragCoefficient => MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE - Math.Min(value / MissionServiceConstants.TelemetryNormalization.MAX_DRAG_COEFFICIENT, MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE),
+                TelemetryFields.LiftCoefficient => Math.Min(value / MissionServiceConstants.TelemetryNormalization.MAX_LIFT_COEFFICIENT, MissionServiceConstants.TelemetryNormalization.MIN_NORMALIZED_VALUE),
                 TelemetryFields.LandingGearStatus => value,
-                TelemetryFields.Latitude => (value + 90.0) / 180.0,
-                TelemetryFields.Longitude => (value + 180.0) / 360.0,
-                _ => 0.5
+                TelemetryFields.Latitude => (value + MissionServiceConstants.TelemetryNormalization.LATITUDE_OFFSET) / MissionServiceConstants.TelemetryNormalization.LATITUDE_RANGE,
+                TelemetryFields.Longitude => (value + MissionServiceConstants.TelemetryNormalization.LONGITUDE_OFFSET) / MissionServiceConstants.TelemetryNormalization.LONGITUDE_RANGE,
+                _ => MissionServiceConstants.TelemetryNormalization.DEFAULT_NORMALIZED_VALUE
             };
         }
     }
