ed40e9b7e16e9fc4b6dd628b33629db0cb581fad
kalush666 <jonathan.kalush@gmail.com>
2025-11-18 08:54:18 +0200

    [feat] algo config class

---
+++ Mission Service/Config/AlgorithmConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Mission_Service.Config
+{
+    public class AlgorithmConfig
+    {
+        public int PopulationSize { get; set; }
+        public int MaxGenerations { get; set; }
+        public double CrossoverProbability { get; set; }
+        public double MutationProbability { get; set; }
+        public double ElitePrecentage { get; set; }
+        public int TornumentSize { get; set; }
+        public int StagnationLimit { get; set; }
+    }
+}
