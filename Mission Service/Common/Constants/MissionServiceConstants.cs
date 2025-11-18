fa7012ac49f538de7786999842fe76f183fc2090
kalush666 <jonathan.kalush@gmail.com>
2025-11-18 09:05:28 +0200

    [feat] config injecting

---
+++ Mission Service/Common/Constants/MissionServiceConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Mission_Service.Common.Constants
+{
+    public static class MissionServiceConstants
+    {
+        public static class Configuration
+        {
+           public const string ALGORITHM_CONFIG_SECTION = "AlgorithmConfig";
+        }
+    }
+}
