42d0edbfb17193a34af996d638095793861ed01c
kalush666 <jonathan.kalush@gmail.com>
2025-11-18 10:16:57 +0200

    [feat] mission model

---
+++ Mission Service/Models/Mission.cs
@@ -0,0 +1,14 @@
+﻿using Core.Models;
+using Mission_Service.Common.Enums;
+
+namespace Mission_Service.Models
+{
+    public class Mission
+    {
+        public string Id { get; set; }
+        public UAVType RequiredUAVType { get; set; }
+        public MissionPriority Priority { get; set; }
+        public TimeWindow TimeWindow { get; set; }
+        public Location Location { get; set; }
+    }
+}
---
+++ Mission Service/Models/TimeWindow.cs
@@ -0,0 +1,17 @@
+﻿namespace Mission_Service.Models
+{
+    public struct TimeWindow
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public TimeWindow(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public TimeWindow()
+        {
+        }
+    }
+}
