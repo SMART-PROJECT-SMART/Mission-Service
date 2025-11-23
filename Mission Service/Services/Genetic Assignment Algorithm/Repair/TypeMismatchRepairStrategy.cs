3fbf64fa2d88aa7eb25856824093ab8d80e1fc70
kalush666 <jonathan.kalush@gmail.com>
2025-11-23 17:29:42 +0200

    [feat] type mismatch repair strategy

---
+++ Mission Service/Services/Genetic Assignment Algorithm/Repair/TypeMismatchRepairStrategy.cs
@@ -0,0 +1,49 @@
+﻿using Mission_Service.Models;
+using Mission_Service.Models.choromosomes;
+
+namespace Mission_Service.Services.Genetic_Assignment_Algorithm.Repair
+{
+    public class TypeMismatchRepairStrategy : IRepairStrategy
+    {
+        public void RepairChromosomeViolation(
+            AssignmentChromosome assignmentChromosome,
+            List<Mission> missions,
+            List<UAV> uavs)
+        {
+            for (int assignmentIndex = assignmentChromosome.Assignments.Count() - 1; assignmentIndex >= 0; assignmentIndex--)
+            {
+                AssignmentGene currentGene = assignmentChromosome.Assignments.ElementAt(assignmentIndex);
+
+                if (IsTypeMismatch(currentGene))
+                {
+                    List<UAV> compatibleUAVs = FindCompatibleUAVs(currentGene.Mission, uavs);
+
+                    if (compatibleUAVs.Count > 0)
+                    {
+                        currentGene.UAV = SelectRandomUAV(compatibleUAVs);
+                    }
+                    else
+                    {
+                        assignmentChromosome.Assignments.RemoveAt(assignmentIndex);
+                    }
+                }
+            }
+        }
+
+        private bool IsTypeMismatch(AssignmentGene gene)
+        {
+            return gene.Mission.RequiredUAVType != gene.UAV.UavType;
+        }
+
+        private List<UAV> FindCompatibleUAVs(Mission mission, List<UAV> uavs)
+        {
+            return uavs.Where(u => u.UavType == mission.RequiredUAVType).ToList();
+        }
+
+        private UAV SelectRandomUAV(List<UAV> compatibleUAVs)
+        {
+            int randomIndex = Random.Shared.Next(compatibleUAVs.Count);
+            return compatibleUAVs[randomIndex];
+        }
+    }
+}
