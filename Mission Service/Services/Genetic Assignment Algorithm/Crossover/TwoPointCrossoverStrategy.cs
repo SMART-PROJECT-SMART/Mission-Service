127603bdaa043d82326b941be54fd3b292c02e7b
kalush666 <jonathan.kalush@gmail.com>
2025-11-25 09:07:36 +0200

    [fix] mis size for selection

--- Mission Service/Services/Genetic Assignment Algorithm/Crossover/TwoPointCrossoverStrategy.cs
+++ Mission Service/Services/Genetic Assignment Algorithm/Crossover/TwoPointCrossoverStrategy.cs
@@ -1,5 +1,6 @@
 ﻿using Mission_Service.Models;
 using Mission_Service.Models.choromosomes;
+using Mission_Service.Common.Constants;
 
 namespace Mission_Service.Services.Genetic_Assignment_Algorithm.Crossover
 {
@@ -7,71 +8,89 @@ namespace Mission_Service.Services.Genetic_Assignment_Algorithm.Crossover
     {
         public CrossoverResult CrossoverChromosomes(
             AssignmentChromosome firstChromosome,
-            AssignmentChromosome secondChromosome
+ AssignmentChromosome secondChromosome
         )
-        {
-            int minLength = Math.Min(
-                firstChromosome.Assignments.Count(),
-                secondChromosome.Assignments.Count()
-            );
-            int firstCrossoverPoint = Random.Shared.Next(1, minLength);
-            int secondCrossoverPoint = Random.Shared.Next(firstCrossoverPoint + 1, minLength + 1);
-            AssignmentChromosome firstChild = CreateChildChromosome(
-                firstChromosome,
-                secondChromosome,
-                firstCrossoverPoint,
-                secondCrossoverPoint,
-                minLength
-            );
-            AssignmentChromosome secondChild = CreateChildChromosome(
-                secondChromosome,
-                firstChromosome,
-                firstCrossoverPoint,
-                secondCrossoverPoint,
-                minLength
+ {
+       int minLength = Math.Min(
+         firstChromosome.Assignments.Count(),
+        secondChromosome.Assignments.Count()
             );
 
-            return new CrossoverResult
-            {
-                FirstChromosome = firstChild,
+            if (minLength < MissionServiceConstants.Crossover.MIN_CHROMOSOMES_FOR_CROSSOVER)
+  {
+           return new CrossoverResult
+     {
+   FirstChromosome = firstChromosome,
+            SecondChromosome = secondChromosome,
+     };
+       }
+
+            int firstCrossoverPoint = Random.Shared.Next(
+            MissionServiceConstants.Crossover.MIN_CROSSOVER_POINT,
+        minLength - 1
+        );
+       int secondCrossoverPoint = Random.Shared.Next(firstCrossoverPoint + 1, minLength);
+
+         AssignmentChromosome firstChild = CreateChildChromosome(
+          firstChromosome,
+        secondChromosome,
+        firstCrossoverPoint,
+  secondCrossoverPoint,
+                minLength
+            );
+   AssignmentChromosome secondChild = CreateChildChromosome(
+secondChromosome,
+       firstChromosome,
+        firstCrossoverPoint,
+              secondCrossoverPoint,
+        minLength
+      );
+
+ return new CrossoverResult
+         {
+     FirstChromosome = firstChild,
                 SecondChromosome = secondChild,
             };
         }
 
         private AssignmentChromosome CreateChildChromosome(
-            AssignmentChromosome firstChromosome,
-            AssignmentChromosome secondChromosome,
+ AssignmentChromosome firstChromosome,
+        AssignmentChromosome secondChromosome,
             int firstCrossoverPoint,
-            int secondCrossoverPoint,
+       int secondCrossoverPoint,
             int length
         )
-        {
+   {
             AssignmentChromosome childChromosome = new AssignmentChromosome();
-            for (int assignmentIndex = 0; assignmentIndex < length; assignmentIndex++)
-            {
-                AssignmentGene sourceGene;
-                if (
-                    assignmentIndex >= firstCrossoverPoint
-                    && assignmentIndex < secondCrossoverPoint
-                )
-                {
-                    sourceGene = secondChromosome.Assignments.ElementAt(assignmentIndex);
-                }
-                else
-                {
-                    sourceGene = firstChromosome.Assignments.ElementAt(assignmentIndex);
-                }
+     List<AssignmentGene> childGenes = new List<AssignmentGene>(length);
 
-                AssignmentGene newGene = new AssignmentGene
+    for (int assignmentIndex = 0; assignmentIndex < length; assignmentIndex++)
+      {
+      AssignmentGene sourceGene;
+          if (
+       assignmentIndex >= firstCrossoverPoint
+      && assignmentIndex < secondCrossoverPoint
+                )
+            {
+        sourceGene = secondChromosome.Assignments.ElementAt(assignmentIndex);
+          }
+      else
                 {
-                    Mission = sourceGene.Mission,
-                    UAV = sourceGene.UAV,
-                    StartTime = sourceGene.StartTime,
-                    Duration = sourceGene.Duration,
-                };
-                childChromosome.Assignments = childChromosome.Assignments.Append(newGene);
-            }
-            return childChromosome;
+   sourceGene = firstChromosome.Assignments.ElementAt(assignmentIndex);
+       }
+
+AssignmentGene newGene = new AssignmentGene
+     {
+              Mission = sourceGene.Mission,
+       UAV = sourceGene.UAV,
+      StartTime = sourceGene.StartTime,
+          Duration = sourceGene.Duration,
+    };
+ childGenes.Add(newGene);
+         }
+
+        childChromosome.Assignments = childGenes;
+return childChromosome;
         }
     }
 }
