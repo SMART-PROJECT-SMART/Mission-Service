9e35760b91c41b196e758ac155ec9460988fa060
kalush666 <jonathan.kalush@gmail.com>
2025-12-14 08:43:03 +0200

    [refactor] location improve

--- Mission Service/Services/Genetic Assignment Algorithm/Fitness/Helpers/LocationProximityCalculator.cs
+++ Mission Service/Services/Genetic Assignment Algorithm/Fitness/Helpers/LocationProximityCalculator.cs
@@ -7,9 +7,7 @@ public static class LocationProximityCalculator
 {
     public static bool IsLocationField(TelemetryFields field)
     {
-        return field == TelemetryFields.Latitude
-            || field == TelemetryFields.Longitude
-            || field == TelemetryFields.Altitude;
+        return field is TelemetryFields.Latitude or TelemetryFields.Longitude or TelemetryFields.Altitude;
     }
 
     public static Location ExtractUAVLocation(Dictionary<TelemetryFields, double> uavTelemetryData)
@@ -23,17 +21,7 @@ public static class LocationProximityCalculator
 
     public static double CalculateProximityScore(Location uavLocation, Location missionLocation)
     {
-        double latitudeDifference = uavLocation.Latitude - missionLocation.Latitude;
-        double longitudeDifference = uavLocation.Longitude - missionLocation.Longitude;
-        double altitudeDifference = uavLocation.Altitude - missionLocation.Altitude;
-
-        double horizontalDistanceSquared =
-            (latitudeDifference * latitudeDifference) + (longitudeDifference * longitudeDifference);
-        double verticalDistanceSquared = altitudeDifference * altitudeDifference;
-
-        double totalDistanceSquared = horizontalDistanceSquared + verticalDistanceSquared;
-        double distance = Math.Sqrt(totalDistanceSquared);
-
+        double distance = uavLocation.CalculateDistanceTo(missionLocation);
         double normalizedProximity = 1.0 / (1.0 + distance);
 
         return normalizedProximity;
